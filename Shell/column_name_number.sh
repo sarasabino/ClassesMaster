8a83191d481d9355c702eb877861f9ebe41be70b
Sara Sabino <sarasabino@gmail.com>
2020-11-21 10:04:42 +0100

    added new commands and shell script

---
+++ Shell/column_name_number.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+FILE_NAME=$1
+DELIMITER=$2
+NUM_OF_COLUMNS=$(head -1 $FILE_NAME| tr $DELIMITER "\n"|wc -l)
+#echo num of columns = $NUM_OF_COLUMNS
+#echo "File name: $FILE_NAME with delimiter=$DELIMITER"
+#paste <(seq $NUM_OF_COLUMNS) <(head -1 optd_aircraft.csv| tr "^" "\n")
+ 
+paste <(seq $NUM_OF_COLUMNS) <(head -1 $FILE_NAME| tr $DELIMITER "\n")
+
